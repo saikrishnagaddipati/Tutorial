9ddd412a4891265b63097c67ccceb29fe682c4c2
ProgrammerPanda <gskrishna44@gmail.com>
2017-04-28 17:02:12 -0700

    Update Thor.rb

--- Thor.rb
+++ Thor.rb
@@ -1,3 +1,5 @@
+# we get the input for below four variables and then we have to move thor who is at @initial_x and @inital_y to the position specified
+#at @light_x and light_y
 @light_x, @light_y, @initial_tx, @initial_ty = gets.split(" ").collect {|x| x.to_i}
 thor_x = @initial_tx
 thor_y = @initial_ty
